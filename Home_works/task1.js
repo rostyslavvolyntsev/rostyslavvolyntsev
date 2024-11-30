784dc7532fb3b0a249f40f2884ded45585f3814d
rostyslavvolyntsev <rostyslavvolyntsev@college.udhtu.edu.ua>
2024-11-30 18:10:42 +0200

    Add files via upload

---
+++ Home_works/task1.js
@@ -0,0 +1,14 @@
+const calculateArea = function(width, height) {
+    return width * height;
+};
+
+let area1 = calculateArea(5, 10); // Викликаємо функцію
+console.log(`Площа прямокутника: ${area1}`); // Виводимо результат
+
+
+
+const calculateAreaArrow = (width, height) => width * height;
+
+let area2 = calculateAreaArrow(5, 10); // Викликаємо функцію
+console.log(`Площа прямокутника: ${area2}`); // Виводимо результат
+
